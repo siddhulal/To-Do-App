c14add94099ce3a0df24b981f3bfdf501be174ab
siddhulal <siddhulal01@gmail.com>
2017-10-17 22:57:12 -0500

    todo app

---
+++ assets/js/todo.js
@@ -0,0 +1,28 @@
+$("ul").on("click", "li", function(){
+    
+	$(this).toggleClass("clicked1");
+});
+
+$("ul").on("click", "span",function(event){
+	$(this).parent().fadeOut(500,function(){
+		$(this).remove();
+	})
+	
+	event.stopPropagation();
+
+});
+
+$("input[type='text']").keypress(function(event){
+    if(event.which===13){
+    	var text=$(this).val();
+    	$(this).val("");
+
+    	$("ul").append("<li><span><i class='fa fa-trash'></i></span> " +text+ "</li");
+
+    }
+});
+
+$(".fa-plus").click(function () {
+	
+	$("input[type='text']").fadeToggle();
+});
